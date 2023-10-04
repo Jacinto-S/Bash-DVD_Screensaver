257f7c891d0d6dafaf673c94a92b49e3b27d4c03
Jacinto-S <122536229+Jacinto-S@users.noreply.github.com>
2023-10-04 15:42:38 +0200

    Update TextAnimation.java
    Finished basic features.

--- TextAnimation.java
+++ TextAnimation.java
@@ -1,54 +1,66 @@
-import java.io.*;
+import java.util.Random;
+import static java.lang.Math.abs;
+
+
 public class TextAnimation {
-  public static int xmax = 237;
-  public static int ymax = 55;
+  public static int R = 255;
+  public static int G = 255;
+  public static int B = 255;
+  public static int xmax = 170;
+  public static int ymax = 61;
   public static int yposup = 7;
   public static int yposdown = 0;
   public static int xposright = 30;
   public static int xposleft = 0;
 
   public static String width = " ";
-  public static String slash = "-";
   public static String newLine = "\n";
     public static void main(String[] args){
-      /*for (int i=0; i<ymax-7; i++){
-	System.out.print("\033[2J");
-	System.out.print(DVD(i*4));
-	System.out.print(newLine.repeat(i));
-	doDelay(48);
-      }*/
-      moveIcon(4, 1);
-      while(true){}
-      /*
-      for (int i=0;i<height;i++){
-      System.out.println(slash.repeat(length));
-      }
-      while(true) {
-	printWithDelay(liste, 19);
-      }
-      */
-  }
-
-  public static void printWithDelay(String[] input, int delay) {
-    for(int i=0; i<input.length; i++) {
-      System.out.print("\033[2J");
-      System.out.print("\n" + input[i]);
-      doDelay(delay);
-    }
+      moveIcon(2, 1);
   }
 
   public static void moveIcon(int xvelocity, int yvelocity) {
-    while (yposup<ymax && xposleft<xmax) {
+    try {
+    while (System.in.available()==0) {
       System.out.print("\033[2J");
-      System.out.print(DVD(yposdown));
-      System.out.print(newLine.repeat(xposleft));
+      System.out.print("\033[38;2;" + R + ";" + G + ";" + B + "m");
+      System.out.print(DVD(xposleft));
+      System.out.print(newLine.repeat(yposdown));
       doDelay(48);
 
-      yposup+=yvelocity;
-      yposdown+=yvelocity;
-      xposright+=xvelocity;
-      xposleft+=xvelocity;
+      yposup += yvelocity;
+      yposdown += yvelocity;
+      xposright += xvelocity;
+      xposleft += xvelocity;
+
+      if (xposleft + xvelocity <= 0) {
+        xvelocity = abs(xvelocity);
+        changeColor();
+      }
+      if (yposdown + yvelocity <= 0) {
+        yvelocity = abs(yvelocity);
+        changeColor();
+      }
+      if (xvelocity + xposright + 1> xmax) {
+        xvelocity = -1 * abs(xvelocity);
+        changeColor();
+      }
+      if (yvelocity + yposup > ymax) {
+        yvelocity = -1 * abs(yvelocity);
+        changeColor();
+      }
     }
+    } catch (Exception e) {
+      System.out.println("Error with end Program loop");
+    }
+    System.out.println("\033[0m");
+  }
+
+  public static void changeColor() {
+    Random rand = new Random();
+    R = rand.nextInt(128) + 128;
+    G = rand.nextInt(128) + 128;
+    B = rand.nextInt(128) + 128;
   }
 
   public static void doDelay(int delay) {
@@ -72,7 +84,7 @@ public class TextAnimation {
     return dvd;
   }
   
-  private static String executeCommand(String command) {
+/*  private static String executeCommand(String command) {
     try {
 	InputStream input = Runtime.getRuntime().exec(command).getInputStream();
 	BufferedReader reader = new BufferedReader(new InputStreamReader(input));
@@ -81,5 +93,5 @@ public class TextAnimation {
 	e.printStackTrace();
 	return null;
     }
-  }
+  }*/
 }
