08cb395a35bdc1a1568ee13535f2342f437d75d5
jschw001 <jacinto.schwarzwaelder@student.hs-rm.de>
2023-10-20 20:46:59 +0200

    Made Background colored if specified in config file

--- src/main/java/Utililties/DVD_Icon.java
+++ src/main/java/Utililties/DVD_Icon.java
@@ -12,25 +12,40 @@ public class DVD_Icon {
     public int R;
     public int G;
     public int B;
-    Settings settings;
+    boolean backgroundIsColored;
+    String resetColor = "\033[0m";
+    String colorCode;
+    int indent;
 
     public DVD_Icon(Settings settings) {
         this.R = 255;
         this.G = 255;
         this.B = 255;
-        this.settings = settings;
+        backgroundIsColored = settings.backgroundIsColored();
     }
 
     public String getIcon(int indent){
-        return
-        //source: https://www.twitchquotes.com/copypastas/4211
-        width.repeat(indent) + "⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⡀\n" +
-        width.repeat(indent) + "⠀⢠⣿⣿⡿⠀⠀⠈⢹⣿⣿⡿⣿⣿⣇⠀⣠⣿⣿⠟⣽⣿⣿⠇⠀⠀⢹⣿⣿⣿ \n" +
-        width.repeat(indent) + "⠀⢸⣿⣿⡇⠀⢀⣠⣾⣿⡿⠃⢹⣿⣿⣶⣿⡿⠋⢰⣿⣿⡿⠀⠀⣠⣼⣿⣿⠏ \n" +
-        width.repeat(indent) + "⠀⣿⣿⣿⣿⣿⣿⠿⠟⠋⠁⠀⠀⢿⣿⣿⠏⠀⠀⢸⣿⣿⣿⣿⣿⡿⠟⠋⠁⠀\n" +
-        width.repeat(indent) + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣸⣟⣁⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
-        width.repeat(indent) + "⣠⣴⣶⣾⣿⣿⣻⡟⣻⣿⢻⣿⡟⣛⢻⣿⡟⣛⣿⡿⣛⣛⢻⣿⣿⣶⣦⣄⡀⠀\n" +
-        width.repeat(indent) + "⠉⠛⠻⠿⠿⠿⠷⣼⣿⣿⣼⣿⣧⣭⣼⣿⣧⣭⣿⣿⣬⡭⠾⠿⠿⠿⠛⠉⠀ ";
+        return createIcon(indent);
+    }
+
+    private String createIcon(int indent) {
+        colorCode = "\033[" + (backgroundIsColored ? "48" : "38") + ";2;" + R + ";" + G + ";" + B + "m";
+        this.indent = indent;
+        return //source: https://www.twitchquotes.com/copypastas/4211
+        createIconLine("⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⡀") +
+        createIconLine("⠀⢠⣿⣿⡿⠀⠀⠈⢹⣿⣿⡿⣿⣿⣇⠀⣠⣿⣿⠟⣽⣿⣿⠇⠀⠀⢹⣿⣿⣿") +
+        createIconLine("⠀⢸⣿⣿⡇⠀⢀⣠⣾⣿⡿⠃⢹⣿⣿⣶⣿⡿⠋⢰⣿⣿⡿⠀⠀⣠⣼⣿⣿⠏") +
+        createIconLine("⠀⣿⣿⣿⣿⣿⣿⠿⠟⠋⠁⠀⠀⢿⣿⣿⠏⠀⠀⢸⣿⣿⣿⣿⣿⡿⠟⠋⠁⠀") +
+        createIconLine("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣸⣟⣁⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀") +
+        createIconLine("⣠⣴⣶⣾⣿⣿⣻⡟⣻⣿⢻⣿⡟⣛⢻⣿⡟⣛⣿⡿⣛⣛⢻⣿⣿⣶⣦⣄⡀⠀") +
+        createIconLine("⠉⠛⠻⠿⠿⠿⠷⣼⣿⣿⣼⣿⣧⣭⣼⣿⣧⣭⣿⣿⣬⡭⠾⠿⠿⠿⠛⠉⠀ ");
+    }
+
+    /**
+     * Transforms a line Text so that it's displayed at the proper location in the terminal.
+     */
+    private String createIconLine(String line) {
+      return width.repeat(indent) + colorCode + line + resetColor + "\n";
     }
 
     public void changeColor() {
@@ -38,6 +53,5 @@ public class DVD_Icon {
         R = rand.nextInt(128) + 128;
         G = rand.nextInt(128) + 128;
         B = rand.nextInt(128) + 128;
-        System.out.print("\033[38;2;" + R + ";" + G + ";" + B + "m");
     }
 }
